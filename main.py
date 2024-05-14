90a0a573997bd5fb6c8ec19766cd209447f7fea1
pterodaktilus <pterodaktilus@github.com>
2024-05-14 06:05:25 +0200

    fixed flickering

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ def translate(val):
     
 cam = cv2.VideoCapture(sys.argv[1]) 
 length = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+time = int(cam.get(cv2.CAP_PROP_POS_MSEC))
 w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frames = zeros((length, h,w), dtype=str)
 i = 0
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
 while(True): 
     # reading from frame 
     ret,frame = cam.read() 
@@ -23,24 +28,27 @@ while(True):
     if ret: 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frames[i]=(translate(frame))
-        if os.name == 'nt':
-            os.system('cls')
-        else:
-            os.system('clear')
+        
         num = "{:.2f}".format(i/(length-1)*100)
         sys.stdout.write(f"translation: {num}%")
+        sys.stdout.write("\033[0;0H")
         sys.stdout.flush()
         i+=1
     else:
         break
 input("\nPress Enter to continue...")
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
 size = frames.shape
 for i in range(size[0]):
+    string = ""
     for j in range(size[1]):
-        sys.stdout.write("".join(frames[i][j].tolist())+"\n")
-        sys.stdout.flush()
-    t.sleep(1/60)
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
+        string+="".join(frames[i][j].tolist())+"\n"
+    sys.stdout.write(string)
+    sys.stdout.flush()
+    t.sleep(length/time)
+    sys.stdout.write("\033[0;0H")
+    sys.stdout.flush()
+
